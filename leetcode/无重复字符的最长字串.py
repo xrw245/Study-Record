d6c6dadd17736271607b8616b0e03d6562f8b616
xrw245 <Wang40621@outlook.com>
2025-12-03 17:25:34 +0800

    Update 无重复字符的最长字串.py

--- leetcode/无重复字符的最长字串.py
+++ leetcode/无重复字符的最长字串.py
@@ -15,4 +15,16 @@ class Solution:
                     break
             str_length_max = len(s_temp) if len(s_temp) > str_length_max else str_length_max
         return str_length_max
+
+# 一次遍历
+# 寻找相同字母的间隔,并将最后一个字母的位置储存在字典中
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dic, res, i = {}, 0, -1
+        for j in range(len(s)):
+            if s[j] in dic:
+                i = max(dic[s[j]], i) # 更新左指针 i
+            dic[s[j]] = j # 哈希表记录
+            res = max(res, j - i) # 更新结果
+        return res
   
